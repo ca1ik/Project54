1b8a0b1e97ecc22d49ad7bdd0b571d918676a0d9
Halil Calik <halicalix@gmail.com>
2024-11-23 00:06:44 +0300

    Update Project22.cpp

--- Project22.cpp
+++ Project22.cpp
@@ -5,7 +5,12 @@ int main() {
  int var = 5;
  // işaretçi (pointer) degisken tanimla
  int* pointVar;
- // var isimli degiskenin adresini sakla
+ // var isimli degiskenin adresini sakla 
+ 
+ 
+ 
+ 
+ 
  pointVar = &var;
  // var degiskeninin degerini yazdir
  cout << "var = " << var << endl;
@@ -16,6 +21,7 @@ int main() {
  // pointVar'ın isaret ettigi adresin icerigini yazdır
  cout << "pointVar'in isaret ettigi adresin icerigi (*pointVar) = "
  << *pointVar << endl;
+ 
  
  system("pause");
  //cikti : var = 5 , var degiskeninin adresi = 0x61ff08 , pointVar = 0x61ff08 , pointVar'in isaret ettigi adresin icerigi (*pointVar) = 5
